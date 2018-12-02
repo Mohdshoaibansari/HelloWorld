f1698bf54e10af04b7ba1212dd49259b6b38b732
Mohd Shoaib <mohammad.ansari586@gmail.com>
2018-12-02 16:43:51 +0530

    Change in File Hello.java and READ

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=0; i<10; i++){
-			System.out.println("Hello work.."+i);
+			System.out.println("Hello new work.."+i);
 			
 		}
 		
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HelloWorld
 This project will be used in Jenkins
+This is the new commit to push.
