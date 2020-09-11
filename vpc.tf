5c31f0ae7b90c666c5c8d92dc6086e09f58e616e
Mohdshoaibansari <43471848+Mohdshoaibansari@users.noreply.github.com>
2020-09-11 22:56:13 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "main_vpc" {
-  source                   = "tf_modules/vpc"
+  source                   = "./tf_modules/vpc"
 
   vpc_cidr_blocks          = vpc_cidr_blocks
   vpc_enable_dns_hostnames = var.vpc["enable_dns_hostnames"]
