8aa2734c9b42da910790a2fe3c62e2793b3dc73f
Mohdshoaibansari <43471848+Mohdshoaibansari@users.noreply.github.com>
2020-09-11 13:17:54 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -1,1 +1,8 @@
-
+provider "aws" {
+  region  = "us-east-1"
+}
+resource "aws_s3_bucket_object" "object1" {
+  bucket  = "yak-forsok"
+  key     = "arch/three_gossips/turret"
+  content = "Delicate"
+}
