3b9f149893f5ff39bd7443ecfdcb57f7b10d86a2
Mohdshoaibansari <43471848+Mohdshoaibansari@users.noreply.github.com>
2020-09-12 12:47:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,3 +14,7 @@ variable "vpc_cidr_blocks_add" {
   type        = string
   description = "The CIDR block for the VPC. Note the first one you define will be the main CIDR block of the VPC and cannot be changed!"
 }
+
+variable "project" {
+  type = string
+}
